35c740222575b63d805086953dd86ee66620eb8f
alt0173 <47327026+alt0173@users.noreply.github.com>
2022-03-14 08:16:31 -0400

    No longer panic if unable to unwrap page contents from epub

--- src/panels/reader.rs
+++ src/panels/reader.rs
@@ -79,128 +79,136 @@ pub fn right_panel_reader_ui(state: &mut MyApp, ui: &mut egui::Ui) {
         } else {
           let style = &state.book_style;
           let theme = &state.theme;
-          let contents = parse_calibre(
-            &book.get_current_str().unwrap(),
-            book.get_current_page(),
-            state.book_userdata.get_mut(selected_book_path).unwrap(),
-          );
-          let contents = contents.lines();
 
-          // Background
-          ui.painter()
-            .rect_filled(ui.clip_rect(), 0.0, theme.page_color);
-
-          // Actual "stuff"
-          let font_id = FontId::new(style.font_size, style.font_family.clone());
-          let line_spacing =
-            ui.fonts().row_height(&font_id) * style.line_spacing_multiplier;
-
-          ui.style_mut().spacing.item_spacing.y = line_spacing;
-
-          let mut goto_target_response = None;
-
-          for (line_number, line) in contents.into_iter().enumerate() {
-            let response = ui.add(
-              Label::new({
-                // Creates text with normal / default appearence
-                // This is how normal body text looks
-                let mut text = RichText::new(line)
-                  .color(theme.text_color)
-                  .background_color(
-                    if let Some(color) = state
-                      .book_userdata
-                      .get(selected_book_path)
-                      .unwrap()
-                      .highlights
-                      .get(&(state.chapter_number, line_number))
-                    {
-                      *color
-                    } else {
-                      Color32::TRANSPARENT
-                    },
-                  )
-                  .font(font_id.clone());
-
-                // Applies special formatting (heading, bold, etc.)
-                if let Some(info) = state
-                  .book_userdata
-                  .get_mut(selected_book_path)
-                  .unwrap()
-                  .formatting_info
-                  .get(&(state.chapter_number, line_number))
-                {
-                  match info {
-                    FormattingInfo::Title => {
-                      text = text.size(font_id.size * 1.75);
-                    }
-                    FormattingInfo::Heading => {
-                      text = text.size(font_id.size * 1.5);
-                    }
-                    FormattingInfo::Heading2 => {
-                      text = text.size(font_id.size * 1.25);
-                    }
-                    FormattingInfo::Bold => text = text.strong(),
-                    FormattingInfo::Italic => text = text.italics(),
-                  }
-                }
-
-                text
-              })
-              .sense(Sense::click()),
+          if let Ok(page_data) = book.get_current_str() {
+            let contents = parse_calibre(
+              &page_data,
+              book.get_current_page(),
+              state.book_userdata.get_mut(selected_book_path).unwrap(),
             );
+            let contents = contents.lines();
 
-            if let Some(target) = &state.goto_target {
-              if line_number == target.line as usize {
-                goto_target_response = Some(response.clone());
-              }
-            }
+            // Background
+            ui.painter()
+              .rect_filled(ui.clip_rect(), 0.0, theme.page_color);
 
-            // Context menu
-            response.context_menu(|ui| {
-              if ui.button("Highlight").clicked() {
-                let highlights = &mut state
-                  .book_userdata
-                  .get_mut(selected_book_path)
-                  .unwrap()
-                  .highlights;
-                let coord = (state.chapter_number, line_number);
+            // Actual "stuff"
+            let font_id =
+              FontId::new(style.font_size, style.font_family.clone());
+            let line_spacing =
+              ui.fonts().row_height(&font_id) * style.line_spacing_multiplier;
 
-                if let Some(color) = highlights.get_mut(&coord) {
-                  if *color == state.theme.highlight_color {
-                    highlights.remove(&coord);
-                  } else {
-                    *color = state.theme.highlight_color;
-                  };
-                } else {
-                  highlights.insert(coord, state.theme.highlight_color);
+            ui.style_mut().spacing.item_spacing.y = line_spacing;
+
+            let mut goto_target_response = None;
+
+            for (line_number, line) in contents.into_iter().enumerate() {
+              let response = ui.add(
+                Label::new({
+                  // Creates text with normal / default appearence
+                  // This is how normal body text looks
+                  let mut text = RichText::new(line)
+                    .color(theme.text_color)
+                    .background_color(
+                      if let Some(color) = state
+                        .book_userdata
+                        .get(selected_book_path)
+                        .unwrap()
+                        .highlights
+                        .get(&(state.chapter_number, line_number))
+                      {
+                        *color
+                      } else {
+                        Color32::TRANSPARENT
+                      },
+                    )
+                    .font(font_id.clone());
+
+                  // Applies special formatting (heading, bold, etc.)
+                  if let Some(info) = state
+                    .book_userdata
+                    .get_mut(selected_book_path)
+                    .unwrap()
+                    .formatting_info
+                    .get(&(state.chapter_number, line_number))
+                  {
+                    match info {
+                      FormattingInfo::Title => {
+                        text = text.size(font_id.size * 1.75);
+                      }
+                      FormattingInfo::Heading => {
+                        text = text.size(font_id.size * 1.5);
+                      }
+                      FormattingInfo::Heading2 => {
+                        text = text.size(font_id.size * 1.25);
+                      }
+                      FormattingInfo::Bold => text = text.strong(),
+                      FormattingInfo::Italic => text = text.italics(),
+                    }
+                  }
+
+                  text
+                })
+                .sense(Sense::click()),
+              );
+
+              if let Some(target) = &state.goto_target {
+                if line_number == target.line as usize {
+                  goto_target_response = Some(response.clone());
                 }
-
-                ui.close_menu();
               }
 
-              if ui.button("Add Note").clicked() {
-                let notes = &mut state
-                  .book_userdata
-                  .get_mut(selected_book_path)
-                  .unwrap()
-                  .notes;
-                let note =
-                  Note::new(book.get_current_page() as u16, line_number as u16);
+              // Context menu
+              response.context_menu(|ui| {
+                if ui.button("Highlight").clicked() {
+                  let highlights = &mut state
+                    .book_userdata
+                    .get_mut(selected_book_path)
+                    .unwrap()
+                    .highlights;
+                  let coord = (state.chapter_number, line_number);
 
-                // Adds the note if one is not already in place for the specified chapter / line combo
-                if !notes.contains(&note) {
-                  notes.push(note);
-                  state.ui_state.left_panel_state = PanelState::Notes;
+                  if let Some(color) = highlights.get_mut(&coord) {
+                    if *color == state.theme.highlight_color {
+                      highlights.remove(&coord);
+                    } else {
+                      *color = state.theme.highlight_color;
+                    };
+                  } else {
+                    highlights.insert(coord, state.theme.highlight_color);
+                  }
 
                   ui.close_menu();
                 }
-              }
-            });
-          }
 
-          if let Some(response) = goto_target_response {
-            response.scroll_to_me(Some(egui::Align::TOP));
-            state.goto_target = None;
+                if ui.button("Add Note").clicked() {
+                  let notes = &mut state
+                    .book_userdata
+                    .get_mut(selected_book_path)
+                    .unwrap()
+                    .notes;
+                  let note = Note::new(
+                    book.get_current_page() as u16,
+                    line_number as u16,
+                  );
+
+                  // Adds the note if one is not already in place for the specified chapter / line combo
+                  if !notes.contains(&note) {
+                    notes.push(note);
+                    state.ui_state.left_panel_state = PanelState::Notes;
+
+                    ui.close_menu();
+                  }
+                }
+              });
+            }
+
+            if let Some(response) = goto_target_response {
+              response.scroll_to_me(Some(egui::Align::TOP));
+              state.goto_target = None;
+            }
+          } else {
+            ui.label("Unable to load page data");
           }
         }
       });
