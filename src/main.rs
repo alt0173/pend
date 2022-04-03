336ee50204af4ba3eb2e25d13c8c399fe9459b96
alt0173 <47327026+alt0173@users.noreply.github.com>
2022-04-02 20:15:29 -0400

    Fixes icon (again)

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn main() {
   let native_options = NativeOptions {
     min_window_size: Some(vec2(960.0, 540.0)),
     icon_data: Some(IconData {
-      rgba: include_bytes!("../compiletime_resources/pend.png").to_vec(),
+      rgba: image::load_from_memory(include_bytes!("../compiletime_resources/pend.png")).unwrap().into_bytes(),
       width: 64,
       height: 64,
     }),
